293012f2a67702dddd65d4d945cd5f8fa7e18932
John <theminor@users.noreply.github.com>
2019-05-14 16:03:08 -0400

    fix detail from soxi

--- xrRecorder.js
+++ xrRecorder.js
@@ -79,9 +79,9 @@ function wsMsg(ws, msg) {
 			if (err) logMsg(`Unable to delete file "${msg.substring(7)}"`, 'error');
 		});
 	} else {  // anything else is assumed to be just a file name - get stats on a given file using soxi
-		childProcess.exec('/usr/bin/soxi ' + FilePath + msg, (sErr, sStOut, sStErr) => {
-console.log(sStOut);
-			//wsSend(ws, JSON.stringify({fileDetail: {fileName: msg, data: cOut}}));
+		childProcess.exec('/usr/bin/soxi ' + FilePath + msg, (sErr, sStOut) => {
+			if (sErr) logMsg(sErr);
+			wsSend(ws, JSON.stringify({fileDetail: {fileName: msg, data: sStOut}}));
 		});
 	}
 	if (msg !== 'getStatus') wsMsg(ws, 'getStatus');
